a9b32c094513988d6353be55a44432d17a46bdf8
Fabio <fabiot92@gmail.com>
2025-03-10 18:20:28 -0300

    Service

--- src/main/java/com/example/demo/enums/StatusEmail.java
+++ src/main/java/com/example/demo/enums/StatusEmail.java
@@ -1,4 +1,7 @@
 package com.example.demo.enums;
 
 public enum StatusEmail {
+    SENT,       // E-mail foi enviado com sucesso
+    ERROR,      // Ocorreu um erro ao tentar enviar o e-mail
+    PENDING     // E-mail está aguardando para ser enviado
 }
--- src/main/java/com/example/demo/services/EmailService.java
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.example.demo.services;
 
+import com.example.demo.enums.StatusEmail;
 import com.example.demo.models.EmailModel;
 import com.example.demo.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,6 +17,23 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
+    @Autowired
+    private JavaMailSender emailSender;
+
     public void sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            emailRepository.save(emailModel);
+        }
     }
 }
